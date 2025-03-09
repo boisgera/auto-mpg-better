484bcc3850df5a5999e1bac8c0d6b4b81fe4d7db
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-03-09 16:24:47 +0100

    Improvements in v5

--- auto-mpg-v5/datasets/check.py
+++ auto-mpg-v5/datasets/check.py
@@ -1,6 +1,21 @@
-import os
-import sys
+import hashlib
+import json
 
-status = os.system("b3sum -c blake3-checksum")
-if status != 0:
-    sys.exit("Error: invalid auto-mpg.parquet file")
+with open("../references.json", encoding="utf-8") as references:
+    bibliography = json.load(references)
+    for item in bibliography:
+        if item["id"] == "auto_mpg_9":
+            auto_mpg_9 = item
+            break
+    else:
+        raise ValueError("auto_mpg_9 reference not found")
+
+hash_info = auto_mpg_9["custom"]["checksum"]
+hash = getattr(hashlib, hash_info["type"])()
+bytes = open("auto-mpg.csv", mode="rb").read()
+hash.update(bytes)
+if hash.hexdigest() != hash_info["value"]:
+    error = f"""auto-mpg.csv checksum mismatch
+- expected: {hash_info["value"]}
+- actual:   {hash.hexdigest()}"""
+    raise ValueError(error)
--- auto-mpg-v5/datasets/fetch.py
+++ auto-mpg-v5/datasets/fetch.py
@@ -3,15 +3,23 @@ import json
 import pandas as pd
 import requests
 
-bib_items = json.load(open("../references.json"))
-for bib_item in bib_items:
-    if bib_item["id"] == "auto_mpg_9":
-        csv_url = bib_item["URL"]
-        assert csv_url.endswith(".csv")
-        break
+with open("../references.json", encoding="utf-8") as references:
+    bibliography = json.load(references)
+    for item in bibliography:
+        if item["id"] == "auto_mpg_9":
+            auto_mpg_9 = item
+            break
+    else:
+        raise ValueError("auto_mpg_9 reference not found")
 
-with open("auto-mpg.csv", "wt", encoding="utf-8") as f:
-    f.write(requests.get(csv_url).text)
-    
+csv_url = auto_mpg_9["URL"]
+assert csv_url.endswith(".csv")
+
+with open("auto-mpg.csv", mode="wt", encoding="utf-8") as csv_file:
+    response = requests.get(csv_url)
+    response.raise_for_status()
+    csv_file.write(response.text)
+
+# Convert CSV to Parquet    
 df = pd.read_csv(csv_url)
 df.to_parquet("auto-mpg.parquet", index=False)
